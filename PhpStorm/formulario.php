e257293240a0b8fdb8f3036dafbf3f3e5bbadc40
Nzhdeh <nzhdeh1991@gmail.com>
2019-10-23 10:16:56 +0200

    formulario

---
+++ PhpStorm/formulario.php
@@ -0,0 +1,11 @@
+<?php
+
+$_nombre = $_GET['nombre'];
+$_email = $_GET['email'];
+
+echo "<h2>Recibimos informacion por php</h2>";
+echo "El nombre es: ". $_nombre . "<br/>";
+echo "El email es: ". $_email . "<br/>";
+
+
+
